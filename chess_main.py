5f9b2ac6a139d37201114c2328b1b42e7afb9e18
MekkCyber <mekk.cyber@gmail.com>
2023-11-08 23:53:38 +0100

    first commit

---
+++ chess_main.py
@@ -0,0 +1,16 @@
+import pygame as p
+
+BOARD_WIDTH = 512
+BOARD_HEIGHT = 512
+MOVE_LOG_PANEL_WIDTH = 250
+MOVE_LOG_PANEL_HEIGHT = BOARD_HEIGHT
+DIMENSION = 8
+SQUARE_SIZE = BOARD_HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
